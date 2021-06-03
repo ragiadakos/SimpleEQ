84ed0264c2d7f7bdb3cdcab4e7b1ade8dc640e77
ragiadakos <ragiadakos@gmail.com>
2021-06-03 19:45:27 +0300

    Implemented Sceleton for slider visuals

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -160,13 +160,22 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 //==============================================================================
 SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor& p)
     : AudioProcessorEditor(&p), audioProcessor(p),
+    peakFreqSlider    (*audioProcessor.apvts.getParameter("Peak Freq"),      "Hz"),
+    peakGainSlider    (*audioProcessor.apvts.getParameter("Peak Gain"),      "dB"),
+    peakQSlider       (*audioProcessor.apvts.getParameter("Peak Q"),         ""),
+    lowCutFreqSlider  (*audioProcessor.apvts.getParameter("LowCut Freq"),    "Hz"),
+    highCutFreqSlider (*audioProcessor.apvts.getParameter("HighCut Freq"),   "Hz"),
+    lowCutSlopeSlider (*audioProcessor.apvts.getParameter("LowCut Slope"),   "db/Oct"),
+    highCutSlopeSlider(*audioProcessor.apvts.getParameter("HighCut Slope" ), "db/Oct"),
+
     responseCurveComponent(audioProcessor),
-    peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
-    peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
-    peakQSliderAttachment(audioProcessor.apvts, "Peak Q", peakQSlider),
-    lowCutFreqSliderAttachment(audioProcessor.apvts, "LowCut Freq", lowCutFreqSlider),
-    highCutFreqSliderAttachment(audioProcessor.apvts, "HighCut Freq", highCutFreqSlider),
-    lowCutSlopeSliderAttachment(audioProcessor.apvts, "LowCut Slope", lowCutSlopeSlider),
+
+    peakFreqSliderAttachment    (audioProcessor.apvts, "Peak Freq",     peakFreqSlider),
+    peakGainSliderAttachment    (audioProcessor.apvts, "Peak Gain",     peakGainSlider),
+    peakQSliderAttachment       (audioProcessor.apvts, "Peak Q",        peakQSlider),
+    lowCutFreqSliderAttachment  (audioProcessor.apvts, "LowCut Freq",   lowCutFreqSlider),
+    highCutFreqSliderAttachment (audioProcessor.apvts, "HighCut Freq",  highCutFreqSlider),
+    lowCutSlopeSliderAttachment (audioProcessor.apvts, "LowCut Slope",  lowCutSlopeSlider),
     highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlopeSlider)
 
 {
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,14 +11,42 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-// struct that helps up build rotary sliders without textbox
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-        juce::Slider::TextEntryBoxPosition::NoTextBox)
-    {
+    void drawRotarySlider(juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override { };
+};
 
+// struct that helps up build rotary sliders without textbox
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+        juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
     }
+
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    void paint(juce::Graphics& g) override { };
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString();
+private:
+    LookAndFeel lnf;
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+
+
 };
 
 struct ResponseCurveComponent : juce::Component
@@ -59,7 +87,7 @@ private:
 
 
     // Create our custom Sliders
-    CustomRotarySlider 
+    RotarySliderWithLabels 
         peakFreqSlider,
         peakGainSlider,
         peakQSlider,
