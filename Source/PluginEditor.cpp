dc4775fed3cea1639c110cf0efd38d368324614c
ragiadakos <ragiadakos@gmail.com>
2021-06-07 05:21:44 +0300

    Display Frequencies

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -368,9 +368,9 @@ void ResponseCurveComponent::resized()
 
     Array<float> freqs
     {
-        20,30,40,50,100,
-        200,300,400,500,1000,
-        2000,3000,4000,5000,10000,
+        20/*,30,40*/,50,100,
+        200/*,300,400*/,500,1000,
+        2000/*,3000,4000*/,5000,10000,
         20000
     };
 
@@ -409,8 +409,39 @@ void ResponseCurveComponent::resized()
         g.drawHorizontalLine(y, left, right);
     }
 
-    g.drawRect(getAnalysisArea());
+    //g.drawRect(getAnalysisArea());
 
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+
+    for (int i = 0; i < freqs.size(); ++i)
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+
+        bool addK = false;
+        String str;
+        if (f > 999.f)
+        {
+            addK = true;
+            f /= 1000.0f;
+        }
+
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+
+        g.drawFittedText(str, r, Justification::centred, 1);
+    }
 }
 
 
