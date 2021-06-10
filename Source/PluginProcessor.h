90839e5c7e2cf563ac673a8ae1a3dc889d377475
ragiadakos <ragiadakos@gmail.com>
2021-06-10 04:23:27 +0300

    Refactored Fifo to separate file

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,73 +9,9 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "Fifo.h"
 
 
-#include<array>
-template<typename T>
-struct Fifo
-{
-    void prepare(int numChannels, int numSamples)
-    {
-        static_assert(std::is_same_v<T, juce::AudioBuffer<float>>,
-            "prepare(numChannels, numSamples) should only be used when the FIFO is holding juce::AudioBuffer<float> data");
-
-
-        for (auto& buffer : buffers)
-        {
-            buffer.setSize(numChannels,
-                numSamples,
-                false, // clear everything
-                true,  // including the extraspace
-                true); // avoid reallocating if you can
-            buffer.clear();
-        }
-    }
-
-    void prepare(size_t numElements)
-    {
-        static_assert(std::is_same_v<T, std::vector<float>>,
-            "prepare(numChannels) should only be used when the FIFO is holding std::vector<float> data");
-         
-        for (auto& buffer : buffers)
-        {
-            buffer.clear();
-            buffer.resize(numElements, 0);
-        }
-
-    }
-
-    bool push(const T& t)
-    {
-        auto write = fifo.write(1);
-        if (write.blockSize1 > 0)
-        {
-            buffers[write.startIndex1] = t;
-            return true;
-        }
-        return false;
-    }
-
-    bool pull(T& t)
-    {
-        auto read = fifo.read(1);
-        if (read.blockSize1 > 0)
-        {
-            t = buffers[read.startIndex1];
-            return true;
-        }
-        return false;
-    }
-
-    int getNumAvailiableForReading() const
-    {
-        return fifo.getNumReady();
-    }
-private:
-    static constexpr int Capacity = 30;
-    std::array<T, Capacity> buffers;
-    juce::AbstractFifo fifo{ Capacity };
-};
 
 // our analyer will display 2 fft curves 1 for each channel
 // we are going to express that programmatically
